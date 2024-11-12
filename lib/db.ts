5e91bd3890958d6454ceaaf6589ad2bc966d05b5
Polar-bear21 <146440233+Polar-bear21@users.noreply.github.com>
2024-11-12 23:57:30 +0900

    DB接続

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,32 @@
-import { createPool } from "mysql2/promise";
+// import { createPool } from "mysql2/promise";
 
-//接続プール
-const pool = createPool({
-  host: process.env.DB_HOST,       // MySQLサーバーのホスト名
-  user: process.env.DB_USER,       // MySQLユーザー名
-  password: process.env.DB_PASSWORD, // MySQLパスワード
-  database: process.env.DB_DATABASE, // 使用するデータベース名
+// //接続プール
+// const pool = createPool({
+//   host: process.env.DB_HOST,       // MySQLサーバーのホスト名
+//   user: process.env.DB_USER,       // MySQLユーザー名
+//   password: process.env.DB_PASSWORD, // MySQLパスワード
+//   database: process.env.DB_DATABASE, // 使用するデータベース名
+// });
+
+// export default pool;
+
+import { Client } from 'pg';
+
+// Supabaseの接続情報を設定
+const client = new Client({
+  user: 'postgres.fjgngzsgyomzgknfgdzm', // Supabaseのユーザー名
+  host: 'aws-0-ap-northeast-1.pooler.supabase.com', // Supabaseのホスト
+  database: 'postgres', // Supabaseのデータベース名
+  password: 'Nippo24c16_', // Supabaseのパスワード
+  port: 6543, // 通常のPostgreSQLのポート番号
 });
 
-export default pool;
+client.connect()
+  .then(() => {
+    console.log('Database connected successfully');
+  })
+  .catch((err) => {
+    console.error('Database connection error', err.stack);
+  });
+
+export default client;
